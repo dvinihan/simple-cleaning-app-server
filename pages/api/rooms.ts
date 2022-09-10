3aecf2d2b3300eca3d9a03dc52efa14efcaf9edd
Daniel Vinihan <daniel.vinihan@gmail.com>
2022-09-10 09:08:53 -0500

    initialize nextjs server

---
+++ pages/api/rooms.ts
@@ -0,0 +1,20 @@
+import clientPromise from "../../util/mongodb";
+
+export default async (req, res) => {
+  try {
+    const client = await clientPromise;
+
+    const data = await client
+      .db("simple-cleaning-app")
+      .collection("rooms")
+      .find()
+      .toArray();
+    const highestId = data.reduce(
+      (max, room) => (room.id > max ? room.id : max),
+      -1
+    );
+    res.send({ rooms: data, nextId: highestId + 1 });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
